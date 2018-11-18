0a7025f948fb13c714fe96fbbd5422a0dc5027d6
arzieba1420 <arzieba@wszib.edu.pl>
2018-11-18 09:57:59 +0100

    2nd commit

---
+++ src/ShowArray.java
@@ -0,0 +1,22 @@
+public class ShowArray {
+    public static void main(String[] args) {
+
+        int[][][] array = {{{1, 2, 3}, {1, 2}}, {{1}}};
+
+
+        for (int k = 0; k < array.length; k++) {
+            for (int j = 0; j < array[k].length; j++) {
+
+                for (int i = 0; i < array[k][j].length; i++) {
+                    System.out.print(array[k][j][i] + "["+k+","+j+","+i+"]"+" ");
+                }
+                System.out.println();
+
+
+            }
+        }
+    }
+
+    }
+
+
